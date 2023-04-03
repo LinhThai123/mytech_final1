cc3a1a13e65282d81ce73ca03d22d0ddafd51352
Linhthai123 <Linhthai130100@gmail.com>
2023-04-03 10:04:47 +0700

    feature : "admin duyệt user đăng ký khóa học đã sửa lỗi "

--- src/main/java/com/example/mytech/controller/admin/UserCourseController.java
+++ src/main/java/com/example/mytech/controller/admin/UserCourseController.java
@@ -1,22 +1,14 @@
 package com.example.mytech.controller.admin;
 
-import com.example.mytech.entity.Course;
 import com.example.mytech.entity.UserCourse;
-import com.example.mytech.model.request.ChangeStatusReq;
-import com.example.mytech.model.request.CourseRep;
 import com.example.mytech.service.UserCourseService;
 import com.example.mytech.websocket.WebSocketHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.validation.Valid;
 
 @Controller
 public class UserCourseController {
@@ -38,13 +30,7 @@ public class UserCourseController {
         model.addAttribute("userCourses", userCourses.getContent());
         model.addAttribute("totalPages", userCourses.getTotalPages());
         model.addAttribute("currentPage", userCourses.getPageable().getPageNumber() + 1);
-        return "admin/course/list_usercourse";
-    }
-
-    @PutMapping("/{id}/status")
-    public ResponseEntity<Object> updateStatus(@PathVariable("id") String id, @RequestBody ChangeStatusReq req) {
-        userCourseService.updateStatus(id, req);
-        return ResponseEntity.ok("Cập nhật thành công");
+        return "admin/user_course/list_usercourse";
     }
 
 }
---
+++ src/main/java/com/example/mytech/controller/api/UserCourseApiController.java
@@ -0,0 +1,38 @@
+package com.example.mytech.controller.api;
+
+import com.example.mytech.entity.UserCourse;
+import com.example.mytech.model.request.ChangeStatusReq;
+import com.example.mytech.repository.UserCourseRepository;
+import com.example.mytech.service.UserCourseService;
+import com.example.mytech.websocket.WebSocketHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Controller
+public class UserCourseApiController {
+
+    @Autowired
+    private UserCourseService userCourseService;
+
+    @Autowired
+    private UserCourseRepository userCourseRepository;
+
+    @Autowired
+    private WebSocketHandler webSocketHandler;
+
+
+    // admin duyệt user đăng ký khóa học
+    @PutMapping("/{id}/status")
+    public ResponseEntity<Object> updateStatus(@PathVariable("id") String id, @RequestBody ChangeStatusReq req) {
+        userCourseService.updateStatus(id, req);
+        webSocketHandler.notifyCourseChange();
+        return ResponseEntity.ok("Cập nhật thành công");
+    }
+}
